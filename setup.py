34e5378052f4fdc6da2c3c632fc1f0a649876378
NicOudart <nicolas.oudart.portable.45@gmail.com>
2023-12-04 19:38:33 +0100

    Bug correction setup file --> missing comma in classifiers.

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ classifiers=[
     'Programming Language :: Python :: 3',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
-    'Intended Audience :: Science/Research'
+    'Intended Audience :: Science/Research',
     'Topic :: Scientific/Engineering :: Astronomy'])
