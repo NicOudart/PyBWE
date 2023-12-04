22d9d38a1ed8333d59bb78ab569a403ed47dcabd
NicOudart <nicolas.oudart.portable.45@gmail.com>
2023-12-04 22:32:54 +0100

    Change the version in the setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(name='PyBWE',
-version='1.0.1',
+version='1',
 description='Python tools for Bandwidth Extrapolation (BWE) of radar data',
 long_description=open("README.md").read(),
 long_description_content_type='text/markdown',
