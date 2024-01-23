385dbc5739d27709042c472bb5a4aa33adddb635
NicOudart <nicolas.oudart.portable.45@gmail.com>
2024-01-23 22:40:20 +0100

    Adding a script for unit tests of the PyBWE package. A corresponding job is also added to the test workflow.

---
+++ test/script_unit_test_BWE.py
@@ -0,0 +1,51 @@
+################################################################################
+#HEADER
+
+#This script allows you to perform a test of unit functions in the PyBWE package.
+
+################################################################################
+
+#Libraries importation:---------------------------------------------------------
+
+import numpy as np
+from math import pi,sqrt
+
+import PyBWE
+
+#Test dataset generation:-------------------------------------------------------
+
+#Generate a vector of 151 frequencies between 1.5 and 3 GHz:
+freq_vect_test = np.linspace(1.5e9,3e9,151)
+
+#Generate a sum of two complex sine-waves corresponding to 2 targets' echoes at
+#the resolution limit, corrupted by a very low amplitude white-noise:
+spec_vect_test = np.exp(-1j*4*pi*freq_vect_test/3e8)+np.exp(-1j*4*pi*1.1*freq_vect_test/3e8)+np.random.normal(0,1e-6,151)
+
+#Reference dataset generation:--------------------------------------------------
+
+#Generate a vector of 451 frequencies between 0 and 2 GHz:
+freq_vect_ref = np.linspace(0,4.5e9,451)
+
+#Generate a sum of two complex sine-waves corresponding to 2 targets' echoes at
+#the resolution limit, corrupted by a very low amplitude white-noise:
+spec_vect_ref = np.exp(-1j*4*pi*freq_vect_ref/3e8)+np.exp(-1j*4*pi*1.1*freq_vect_ref/3e8)+np.random.normal(0,1e-6,451)
+
+#Test function burg:------------------------------------------------------------
+
+#Application of the burg function to the test dataset:
+a,e,rc = PyBWE.burg(spec_vect_test,2)
+
+#Check the AR model's coefficients:
+assert (abs(abs(a[0])-2)<1e-3)and(abs(abs(a[1])-1)<1e-3), 'PyBWE.burg NOK!'
+
+#Test function ar_extrapolation:------------------------------------------------
+
+#Application of the ar_extrapolation function to the test dataset:
+x_extra,x_forward,x_backward = PyBWE.ar_extrapolation(spec_vect_test,a,150,'both')
+
+#Check the extrapolated spectrum:
+assert sqrt(sum(abs(spec_vect_ref-x_extra)**2)/300)<1e-3, 'PyBWE.extrapolated NOK!'
+
+#Output:------------------------------------------------------------------------
+
+print('PyBWE unit tests OK!')
